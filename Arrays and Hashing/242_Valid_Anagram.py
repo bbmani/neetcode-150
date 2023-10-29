548016242e297624f02f4edd7803d7e02c976e67
bbmani <balajibharatwajmanikandan>
2023-10-29 10:05:23 -0400

    Complete 1: 242 Valid Anagram

--- Arrays and Hashing/242_Valid_Anagram.py
+++ Arrays and Hashing/242_Valid_Anagram.py
@@ -1,10 +1,17 @@
-def isAnagram(s, t):
-	return list(set(s)) == list(set(t))
+def isAnagram(s,t) : 
+    s = list(s)
+    t = list(t)
+    
+    for x in t : 
+        if x in s :
+            s.pop(s.index(x))
+            
+    return len(s) == 0
 
 def main():
 	questions = [["anagram","nagaram"], ["rat", "car"]]
 	for x in questions : 
-		print(isAnagram(questions[0], questions[1]))
+		print(isAnagram(x[0], x[1]))
 
 if __name__ == "__main__" : 
 	main()
